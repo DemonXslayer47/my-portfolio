b015eb380f1fa0e0ed01c47416a11ee13a51ab56
sthota3 <sreehari.thota@slu.edu>
2024-06-28 17:42:50 -0500

    Added changes

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import ContactForm from "./ContactForm";
-import ContactImage from "../../Assets/contact.svg"; 
+import ContactImage from "../../Assets/contact.svg";
 
 function Contact() {
   return (
     <Container fluid className="contact-section">
       <Particle />
       <Container>
-        <Row className="align-items-center">
-          <Col md={6}>
+        <Row className="contact-content">
+          <Col md={6} className="contact-form">
             <ContactForm />
           </Col>
-          <Col md={6} style={{ textAlign: "center" }}>
-            <img src={ContactImage} alt="Contact" className="contact-image" /> 
+          <Col md={6} className="contact-image">
+            <img src={ContactImage} alt="Contact" />
           </Col>
         </Row>
       </Container>
