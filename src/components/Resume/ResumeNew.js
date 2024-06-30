16845998c30ddea6c25ffb84c4ff25c4ecb250af
sthota3 <sreehari.thota@slu.edu>
2024-06-29 19:51:14 -0500

    Added Latest Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -40,7 +40,7 @@ function ResumeNew() {
           <Document
             file={pdf}
             onLoadSuccess={onDocumentLoadSuccess}
-            className="d-flex justify-content-center"
+            className="d-flex flex-column align-items-center"
           >
             {Array.from(new Array(numPages), (el, index) => (
               <Page
